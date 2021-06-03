9644074030937853a3d8369322b51ddd7e922e33
RundesBalli <GitHub@RundesBalli.com>
2021-06-04 00:20:53 +0200

    Dateien hinzugefügt

---
+++ public/inc/datasafety.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * datasafety.php
+ * 
+ * Datenschutzerklärung
+ */
+$title = "Datenschutzerklärung";
+
+$content.= "<h1>Datenschutzerklärung</h1>".PHP_EOL;
+
+?>
---
+++ public/inc/info.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * info.php
+ * 
+ * Informationsseite
+ */
+$title = "Informationsseite";
+
+$content.= "<h1>Informationsseite</h1>".PHP_EOL;
+
+?>

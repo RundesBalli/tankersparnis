f4aa4a17e30ec818fa2281c0be403e4abbbc8144
RundesBalli <GitHub@RundesBalli.com>
2021-06-06 16:06:55 +0200

    Userlog Funktion hinzugefügt

--- public/inc/functions.php
+++ public/inc/functions.php
@@ -31,4 +31,59 @@ function defuse($defuse_string, $trim = TRUE) {
 function output($string) {
   return htmlentities($string, ENT_QUOTES);
 }
+
+/**
+ * Userlog Funktion
+ * Zum loggen aller User Handlungen
+ * 
+ * @param int    $userId      userId des Users
+ * @param int    $logLevel    logLevel der Aktion
+ * @param string $text        optionaler Text
+ */
+function userLog($userId = NULL, int $logLevel, $text = NULL) {
+  global $dbl;
+
+  /**
+   * Prüfung, ob die userId existiert. Falls nicht wird sie genullt.
+   */
+  if($userId !== NULL) {
+    $userId = (int)defuse($userId);
+    $result = mysqli_query($dbl, "SELECT `id` FROM `users` WHERE `id`='".$userId."' LIMIT 1") OR DIE(MYSQLI_ERROR($dbl));
+    if(mysqli_num_rows($result) != 1) {
+      $userId = NULL;
+    }
+  } else {
+    $userId = NULL;
+  }
+
+  /**
+   * Prüfung ob das logLevel existiert. Falls nicht, wird es auf "User-/Systemaktion" (1) gesetzt.
+   */
+  if(is_int($logLevel)) {
+    $logLevel = defuse($logLevel);
+    $result = mysqli_query($dbl, "SELECT `id` FROM `logLevel` WHERE `id`='".$logLevel."' LIMIT 1") OR DIE(MYSQLI_ERROR($dbl));
+    if(mysqli_num_rows($result) != 1) {
+      $logLevel = 1;
+    }
+  } else {
+    $logLevel = 1;
+  }
+
+  /**
+   * Entschärfen des Textes, sofern vorhanden.
+   */
+  if($text !== NULL) {
+    $text = defuse($text);
+  }
+
+  /**
+   * Eintragen ins Log
+   */
+  mysqli_query($dbl, "INSERT INTO `log` (`userId`, `logLevel`, `text`) VALUES (".($userId !== NULL ? "'".$userId."'" : "NULL").", '".$logLevel."', ".($text !== NULL ? "'".$text."'" : "NULL").")") OR DIE(MYSQLI_ERROR($dbl));
+  if(mysqli_affected_rows($dbl) != 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 ?>

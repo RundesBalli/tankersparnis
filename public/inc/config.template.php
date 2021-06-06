195fa1d33fe57fcef7f9a3c7c0bda5f79eb1124d
RundesBalli <GitHub@RundesBalli.com>
2021-06-06 22:02:33 +0200

    Config um Zeitzone und Mail-Einstellungen erweitert

--- public/inc/config.template.php
+++ public/inc/config.template.php
@@ -21,8 +21,47 @@ $dbl = mysqli_connect(
 mysqli_set_charset($dbl, "utf8") OR DIE(MYSQLI_ERROR($dbl));
 
 /**
+ * Zeitzoneneinstellung
+ * @param string Zeitzone
+ */
+date_default_timezone_set("Europe/Berlin");
+
+/**
  * Login Cookie
  * @var string Name des Cookies
  */
 $cookieName = "";
+
+/**
+ * Mail Einstellungen
+ * @var string Hostname des Mailservers
+ * @var int Port des Mailservers
+ * @var string SMTP Username
+ * @var string SMTP Passwort
+ * 
+ * @var string Angezeigte "Von:" E-Mail Adresse
+ * @var string Angezeigter "Von:" Name
+ * @var string Abschließende Grußformel am Ende jeder E-Mail
+ */
+$mailConfig['conn']['host'] = "mail.example.com";
+$mailConfig['conn']['port'] = 587;
+$mailConfig['conn']['smtpUser'] = "noreply@example.com";
+$mailConfig['conn']['smtpPass'] = "";
+
+$mailConfig['conf']['fromEmail'] = "noreply@example.com";
+$mailConfig['conf']['fromName'] = "Example.com";
+$mailConfig['conf']['closingGreeting'] = "Viele Grüße\nDein Team von Example.com";
+
+/**
+ * Mail Betreffe
+ * @var string Betreff der Registrierungsmail
+ * @var string Betreff der Aktivierungsmail (nach Klicken des Aktivierungslinks in der Registrierungsmail)
+ * @var string Betreff der "Passwort vergessen" Mail
+ * @var string Betreff der "Passwort wurde zurückgesetzt" Mail
+ */
+$mailConfig['subject']['register'] = "Deine Registrierung";
+$mailConfig['subject']['accountActivated'] = "Dein Account wurde aktiviert";
+$mailConfig['subject']['forgotPassword'] = "Passwort vergessen";
+$mailConfig['subject']['passwordResetted'] = "Passwort wurde zurückgesetzt";
+
 ?>

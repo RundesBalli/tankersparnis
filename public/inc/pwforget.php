a5e24dbf4719f089af73d4c1f63bf7ca46bce2df
RundesBalli <GitHub@RundesBalli.com>
2021-06-06 16:09:15 +0200

    Passwort Vergessen Seite angelegt, Inhalt folgt

---
+++ public/inc/pwforget.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * pwforget.php
+ * 
+ * Passwort vergessen Funktion
+ */
+$title = "Passwort vergessen";
+
+$content.= "<h1><span class='fas icon'>&#xf084;</span>Passwort vergessen</h1>".PHP_EOL;
+
+/**
+ * Prüfen ob eingeloggt. Wenn ja, dann Umleitung auf Nutzerseite. Prüfung auf Validität erfolgt über die Userseite.
+ */
+if((isset($_COOKIE[$cookieName]) AND !empty($_COOKIE[$cookieName])) AND preg_match('/[a-f0-9]{64}/i', defuse($_COOKIE[$cookieName]), $match) === 1) {
+  header("Location: /overview");
+  die();
+}
+
+
+?>

6c1e10700d4a016564dfdab25428175804ea9db6
RundesBalli <GitHub@RundesBalli.com>
2021-06-18 10:09:36 +0200

    Ersparnisseite hinzugefügt

---
+++ public/inc/savings.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * savings.php
+ * 
+ * Detailansicht der Ersparnisse
+ */
+
+/**
+ * Einbinden der Cookieüberprüfung.
+ */
+require_once('cookiecheck.php');
+
+/**
+ * Titel und Überschrift
+ */
+$title = "Ersparnisse";
+$content.= "<h1><span class='fas icon'>&#xf153;</span>Ersparnisse</h1>";
+
+$content.= "<div class='row breakWord small'>".
+  "<div class='col-s-12 col-l-0 bold highlight'>Hinweis!</div>".
+  "<div class='col-s-12 col-l-0'>Für eine detailliertere Ansicht musst du diese Seite von einem Computer aus aufrufen oder in den \"Desktop-Modus\" in deinem Handybrowser wechseln!</div>".
+  "<div class='col-s-12 col-l-0 spacer-m'></div>".
+"</div>";
+

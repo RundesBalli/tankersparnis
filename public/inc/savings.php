35dfd111fe041b67a578c4097f1eea3331ccf705
RundesBalli <GitHub@RundesBalli.com>
2021-06-18 11:07:06 +0200

    Mobilen Hinweistext verschoben, Vorschlagstext erweitert

--- public/inc/savings.php
+++ public/inc/savings.php
@@ -16,12 +16,6 @@ require_once('cookiecheck.php');
 $title = "Ersparnisse";
 $content.= "<h1><span class='fas icon'>&#xf153;</span>Ersparnisse</h1>";
 
-$content.= "<div class='row breakWord small'>".
-  "<div class='col-s-12 col-l-0 bold highlight'>Hinweis!</div>".
-  "<div class='col-s-12 col-l-0'>Für eine detailliertere Ansicht musst du diese Seite von einem Computer aus aufrufen oder in den \"Desktop-Modus\" in deinem Handybrowser wechseln!</div>".
-  "<div class='col-s-12 col-l-0 spacer-m'></div>".
-"</div>";
-
 /**
  * Anzeigevarianten
  */
@@ -31,9 +25,15 @@ $content.= "<div class='row'>".
   "<div class='col-s-12 col-l-12'><a href='/savings?view=monthly'>Monatswerte</a></div>".
   "<div class='col-s-12 col-l-12'><a href='/savings?view=maxSaved'>Am meisten gespart (Betrag)</a></div>".
   "<div class='col-s-12 col-l-12'><a href='/savings?view=minUsed'>Am wenigsten verbraucht (100km)</a></div>".
-  "<div class='col-s-12 col-l-12 small'>Vorschläge für weitere Anzeigevarianten gerne per <a href='mailto:info@tankersparnis.net'>Mail</a> oder <a href='https://github.com/RundesBalli/tankersparnis/issues' target='_blank' rel='noopener'>Issue</a>.</div>".
+  "<div class='col-s-12 col-l-12 small'>Vorschläge für weitere Anzeigevarianten gerne per <a href='mailto:info@tankersparnis.net'>Mail</a>, <a href='https://github.com/RundesBalli/tankersparnis/issues' target='_blank' rel='noopener'>Issue</a> oder <a href='https://github.com/RundesBalli/tankersparnis/pulls' target='_blank' rel='noopener'>PR</a>.</div>".
 "</div>";
 $content.= "<div class='spacer-m'></div>";
+
+$content.= "<div class='row breakWord'>".
+  "<div class='col-s-12 col-l-0 bold highlight'>Hinweis!</div>".
+  "<div class='col-s-12 col-l-0'>Für eine detailliertere Ansicht musst du diese Seite von einem Computer aus aufrufen oder in den \"Desktop-Modus\" in deinem Handybrowser wechseln!</div>".
+  "<div class='col-s-12 col-l-0 spacer-m'></div>".
+"</div>";
 
 if($_GET['view'] == 'total') {
   /**

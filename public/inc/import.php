4e0d13fb60a7cb3411dd05763e8f3cc492391bbe
RundesBalli <GitHub@RundesBalli.com>
2022-02-02 20:51:58 +0100

    Email Anzeige hinzugefügt. Unterstützte Imports angepasst.

--- public/inc/import.php
+++ public/inc/import.php
@@ -19,14 +19,14 @@ $content.= "<h1><span class='fas icon'>&#xf56f;</span>Importieren</h1>";
 $content.= "<div class='row'>".
   "<div class='col-s-12 col-l-12'>Es besteht die Möglichkeit, Daten von einer anderen Plattform hier her zu importieren.</div>".
   "<div class='col-s-12 col-l-12'>Solltest du die Möglichkeit haben einen Datendump/Export von der anderen Seite herunterzuladen, dann kannst du ihn an <a href='mailto:info@tankersparnis.net?subject=Datenimport'>info@tankersparnis.net</a> senden.</div>".
-  "<div class='col-s-12 col-l-12'>Bitte achte darauf, dass du die E-Mail von der Mail-Adresse schickst, mit der du hier registriert bist.</div>".
+  "<div class='col-s-12 col-l-12'>Bitte achte darauf, dass du die E-Mail von der Mail-Adresse schickst, mit der du hier registriert bist <span class='small'>(".output($email).")</span>.</div>".
 "</div>";
 $content.= "<div class='spacer-m'></div>";
 
 $content.= "<h2>Bisher unterstützte Imports:</h2>";
 $content.= "<div class='row'>".
-  "<div class='col-s-12 col-l-12'>Deine eigene Excel-Tabelle oder CSV-Datei</div>".
-  "<div class='col-s-12 col-l-12'>CSV Export von Spritmonitor.de</div>".
+  "<div class='col-s-12 col-l-12'><span class='fas icon'>&#xf35a;</span>Deine eigene Excel-Tabelle, CSV-Datei oder andere Datenaufstellung <span class='small'>(bitte keine abfotografierte, handschriftliche Liste auf Kachelpapier)</span></div>".
+  "<div class='col-s-12 col-l-12'><span class='fas icon'>&#xf35a;</span>CSV Export von Spritmonitor.de</div>".
   "<div class='col-s-12 col-l-12 small italic'><span class='highlight bold'>Hinweis:</span> Sollte deine Plattform nicht aufgeführt sein, dann macht das nichts. Schicke uns einfach den Export und wir versuchen es zu importieren.</div>".
 "</div>";
 $content.= "<div class='spacer-m'></div>";
